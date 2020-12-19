cc1b92e542ed7b876b7b79b22fd2b236974f98b3
imirsaburov <imirsaburov@gmail.com>
2020-12-20 00:36:47 +0500

    WebSocket

--- server/src/main/resources/static/layouts__index.async.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[1],{A0qh:function(n,t,e){n.exports={normal:"normal___2M-6v",title:"title___7IVVB"}},aArQ:function(n,t,e){"use strict";e.r(t);var r=e("q1tI"),i=e.n(r);e("A0qh");function o(n){return i.a.createElement("div",null,n.children)}t["default"]=o}}]);
